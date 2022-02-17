66b0ddb5d88b4b23a9d3ce9f8a37f36aaa2e8820
professordarug <professordarug@gmail.com>
2022-02-17 18:56:49 -0300

    Criado o padrao da extrutura para os JS

---
+++ js01/javascript.js
@@ -0,0 +1,8 @@
+// funções / tarefas / rotinas
+
+//algo acontece ou pode ser automatizada para ter a chamada
+//parametro é o dinheiro
+//toda função tem uma ação
+//toda função tem retorno
+
+//são ações executadas assim que sação chamadas ou em decorrencia de algum Evento
